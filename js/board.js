6fbbcbce30a2745d4c689ee7e74656b60a9b7894
sharmin133 <mstsharmin1781@gmail.com>
2025-03-01 23:59:57 +0600

    added four

--- js/board.js
+++ js/board.js
@@ -3,8 +3,17 @@ let total=23;
 
 let allTotal=6;
 
-document.getElementById('btn-one').addEventListener('click',function(){
+ let buttons=document.querySelectorAll('.btn-one');
+ for(i=0; i<buttons.length; i++){
+ buttons[i].addEventListener('click',updateBoard);
+ }
+    function updateBoard(){
     alert('Board updated Successfully');
+
+    if(allTotal===1){
+        alert('congrates!!! You have completed all the current task');
+    }
+
     total++;
     document.getElementById('total-increase').textContent= total;
 
@@ -12,12 +21,13 @@ document.getElementById('btn-one').addEventListener('click',function(){
     document.getElementById('total-decrease').textContent= allTotal;
     document.getElementById('btn-one').disabled=true;
    
-})
+}
+ 
 
 
 
 
-const colors=['red','green', 'blue', 'purple', 'yellow', 'black', 'pink','lime','orange','gray','lightblue',];
+const colors=['red','green', 'blue', 'purple', 'yellow', 'black', 'pink','lime','orange','gray','lightblue'];
 let currentIn=0;
 function changeBackgroundColor(){
     document.body.style.backgroundColor=colors[currentIn];
